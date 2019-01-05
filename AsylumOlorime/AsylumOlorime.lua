0da9da60f9c157dcf85110909f121ea39699532f
Floliroy <florian.leeroy@hotmail.com>
2019-01-05 17:03:36 +0100

    code improvment

--- AsylumOlorime/AsylumOlorime.lua
+++ AsylumOlorime/AsylumOlorime.lua
@@ -9,18 +9,21 @@ AsylumOlorime.version = "1.0"
 ---------------------------
 ---- Variables Default ----
 ---------------------------
+
+
 AsylumOlorime.Default = {
 	OffsetX = 510,
     OffsetY = 240,
-	DPS1 = "@Floliroy",
-	DPS2 = "@everyone",
-	DPS3 = "@everyone",
-	DPS4 = "@everyone",
-	DPS5 = "@everyone",
-	DPS6 = "@everyone",
-	DPS7 = "@everyone",
-	DPS8 = "@everyone",
-	ColorRGB = {0.58, 0.52, 0.02, 1}
+	ColorRGB = {0, 1, 0, 1},
+	DPS = {	[1] = "@Floliroy",
+			[2] = "@everyone",
+			[3] = "@everyone",
+			[4] = "@everyone",
+			[5] = "@everyone",
+			[6] = "@everyone",
+			[7] = "@everyone",
+			[8] = "@everyone",
+	},
 }
 
 -------------------------
@@ -47,94 +50,94 @@ function AsylumOlorime.CreateSettingsWindow()
 		},
 		[2] = {
 			type = "description",
-			text = "Just set the @name of your 8 DDs.",
+			text = "Just set the @name of your 8 DDs.\n\r Position 1 is entrance, position 8 is exit.",
 		},
 		[3] = {
 			type = "editbox",
 			name = "DD Position 1",
 			tooltip = "The @name of the DD in position 1",
-			default = AsylumOlorime.Default.DPS1,
-			getFunc = function() return AsylumOlorime.savedVariables.DPS1 end,
+			default = AsylumOlorime.Default.DPS[1],
+			getFunc = function() return AsylumOlorime.savedVariables.DPS[1] end,
 			setFunc = function(newValue) 
-				AsylumOlorime.savedVariables.DPS1 = newValue
-				AsylumOlorime.DPS1 = newValue
+				AsylumOlorime.savedVariables.DPS[1] = newValue
+				AsylumOlorime.DPS[1] = newValue
 				end,
 	   },
 	   [4] = {
             type = "editbox",
             name = "DD Position 2",
 			tooltip = "The @name of the DD in position 2",
-			default = AsylumOlorime.Default.DPS2,
-            getFunc = function() return AsylumOlorime.savedVariables.DPS2 end,
+			default = AsylumOlorime.Default.DPS[2],
+            getFunc = function() return AsylumOlorime.savedVariables.DPS[2] end,
 			setFunc = function(newValue) 
-				AsylumOlorime.savedVariables.DPS2 = newValue
-				AsylumOlorime.DPS2 = newValue
+				AsylumOlorime.savedVariables.DPS[2] = newValue
+				AsylumOlorime.DPS[2] = newValue
 				end,
 		},
 		[5] = {
 			type = "editbox",
 			name = "DD Position 3",
 			tooltip = "The @name of the DD in position 3",
-			default = AsylumOlorime.Default.DPS3,
-			getFunc = function() return AsylumOlorime.savedVariables.DPS3 end,
+			default = AsylumOlorime.Default.DPS[3],
+			getFunc = function() return AsylumOlorime.savedVariables.DPS[3] end,
 			setFunc = function(newValue) 
-				AsylumOlorime.savedVariables.DPS3 = newValue
-				AsylumOlorime.DPS3 = newValue
+				AsylumOlorime.savedVariables.DPS[3] = newValue
+				AsylumOlorime.DPS[3] = newValue
 				end,
 		},
 	   [6] = {
 			type = "editbox",
 			name = "DD Position 4",
 			tooltip = "The @name of the DD in position 4",
-			default = AsylumOlorime.Default.DPS4,
-			getFunc = function() return AsylumOlorime.savedVariables.DPS4 end,
+			default = AsylumOlorime.Default.DPS[4],
+			getFunc = function() return AsylumOlorime.savedVariables.DPS[4] end,
 			setFunc = function(newValue) 
-				AsylumOlorime.savedVariables.DPS4 = newValue
-				AsylumOlorime.DPS4 = newValue
+				AsylumOlorime.savedVariables.DPS[4] = newValue
+				AsylumOlorime.DPS[4] = newValue
 				end,
 		},
 		[7] = {
 			type = "editbox",
 			name = "DD Position 5",
 			tooltip = "The @name of the DD in position 5",
-			default = AsylumOlorime.Default.DPS5,
-			getFunc = function() return AsylumOlorime.savedVariables.DPS5 end,
+			default = AsylumOlorime.Default.DPS[5],
+			getFunc = function() return AsylumOlorime.savedVariables.DPS[5] end,
 			setFunc = function(newValue) 
-				AsylumOlorime.savedVariables.DPS5 = newValue
-				AsylumOlorime.DPS5 = newValue
+				AsylumOlorime.savedVariables.DPS[5] = newValue
+				AsylumOlorime.DPS[5] = newValue
 				end,
 	   },
 	   [8] = {
             type = "editbox",
             name = "DD Position 6",
 			tooltip = "The @name of the DD in position 6",
-			default = AsylumOlorime.Default.DPS6,
-            getFunc = function() return AsylumOlorime.savedVariables.DPS6 end,
+			default = AsylumOlorime.Default.DPS[6],
+            getFunc = function() return AsylumOlorime.savedVariables.DPS[6] end,
 			setFunc = function(newValue) 
-				AsylumOlorime.savedVariables.DPS6 = newValue
-				AsylumOlorime.DPS6 = newValue
+				AsylumOlorime.savedVariables.DPS[6] = newValue
+				AsylumOlorime.DPS[6] = newValue
 				end,
 		},
 		[9] = {
 			type = "editbox",
 			name = "DD Position 7",
 			tooltip = "The @name of the DD in position 7",
-			default = AsylumOlorime.Default.DPS7,
-			getFunc = function() return AsylumOlorime.savedVariables.DPS7 end,
+			default = AsylumOlorime.Default.DPS[7],
+			getFunc = function() return AsylumOlorime.savedVariables.DPS[7] end,
 			setFunc = function(newValue) 
-				AsylumOlorime.savedVariables.DPS7 = newValue
-				AsylumOlorime.DPS7 = newValue
+				AsylumOlorime.savedVariables.DPS[7] = newValue
+				AsylumOlorime.DPS[7] = newValue
 				end,
 		},
 	   [10] = {
 			type = "editbox",
 			name = "DD Position 8",
 			tooltip = "The @name of the DD in position 8",
-			default = AsylumOlorime.Default.DPS8,
-			getFunc = function() return AsylumOlorime.savedVariables.DPS8 end,
+			default = AsylumOlorime.Default.DPS[8],
+			getFunc = function() return AsylumOlorime.savedVariables.DPS[8] end,
 			setFunc = function(newValue) 
-				AsylumOlorime.savedVariables.DPS8 = newValue
-				AsylumOlorime.DPS8 = newValue
+				AsylumOlorime.savedVariables.DPS[8] = newValue
+				AsylumOlorime.DPS[8] = newValue
 				end,
 		},
 		[11] = {
@@ -160,70 +163,32 @@ function AsylumOlorime.Update()
 	local left = 0;
 	local right = 0;
 
+	--d(GetUnitName(AsylumOlorime.DPS[1]))
+
 	for playerID = 1, GetGroupSize() do
-		--right DDs
-		if (GetUnitDisplayName(GetGroupUnitTagByIndex(playerID)) == AsylumOlorime.DPS1) then
-			for i=1,GetNumBuffs(GetGroupUnitTagByIndex(playerID)) do
-				local buffName, timeStarted, timeEnding, buffSlot, stackCount, iconFilename, buffType, effectType, abilityType, statusEffectType, abilityId, canClickOff, castByPlayer = GetUnitBuffInfo(GetGroupUnitTagByIndex(playerID),i)
-				if ((abilityId == 109994) or (abilityId == 110020)) then
-					right = right + 1.9
-				end
-			end
-		end
-		if (GetUnitDisplayName(GetGroupUnitTagByIndex(playerID)) == AsylumOlorime.DPS2) then
-			for i=1,GetNumBuffs(GetGroupUnitTagByIndex(playerID)) do
-				local buffName, timeStarted, timeEnding, buffSlot, stackCount, iconFilename, buffType, effectType, abilityType, statusEffectType, abilityId, canClickOff, castByPlayer = GetUnitBuffInfo(GetGroupUnitTagByIndex(playerID),i)
-				if ((abilityId == 109994) or (abilityId == 110020)) then
-					right = right + 1.6
-				end
-			end
-		end
-		if (GetUnitDisplayName(GetGroupUnitTagByIndex(playerID)) == AsylumOlorime.DPS3) then
-			for i=1,GetNumBuffs(GetGroupUnitTagByIndex(playerID)) do
-				local buffName, timeStarted, timeEnding, buffSlot, stackCount, iconFilename, buffType, effectType, abilityType, statusEffectType, abilityId, canClickOff, castByPlayer = GetUnitBuffInfo(GetGroupUnitTagByIndex(playerID),i)
-				if ((abilityId == 109994) or (abilityId == 110020)) then
-					right = right + 1.3
-				end
-			end
-		end
-		if (GetUnitDisplayName(GetGroupUnitTagByIndex(playerID)) == AsylumOlorime.DPS4) then
-			for i=1,GetNumBuffs(GetGroupUnitTagByIndex(playerID)) do
-				local buffName, timeStarted, timeEnding, buffSlot, stackCount, iconFilename, buffType, effectType, abilityType, statusEffectType, abilityId, canClickOff, castByPlayer = GetUnitBuffInfo(GetGroupUnitTagByIndex(playerID),i)
-				if ((abilityId == 109994) or (abilityId == 110020)) then
-					right = right + 1
-				end
-			end
-		end
-		--left DDs
-		if (GetUnitDisplayName(GetGroupUnitTagByIndex(playerID)) == AsylumOlorime.DPS5) then
-			for i=1,GetNumBuffs(GetGroupUnitTagByIndex(playerID)) do
-				local buffName, timeStarted, timeEnding, buffSlot, stackCount, iconFilename, buffType, effectType, abilityType, statusEffectType, abilityId, canClickOff, castByPlayer = GetUnitBuffInfo(GetGroupUnitTagByIndex(playerID),i)
-				if ((abilityId == 109994) or (abilityId == 110020)) then
-					left = left + 1
-				end
-			end
-		end
-		if (GetUnitDisplayName(GetGroupUnitTagByIndex(playerID)) == AsylumOlorime.DPS6) then
-			for i=1,GetNumBuffs(GetGroupUnitTagByIndex(playerID)) do
-				local buffName, timeStarted, timeEnding, buffSlot, stackCount, iconFilename, buffType, effectType, abilityType, statusEffectType, abilityId, canClickOff, castByPlayer = GetUnitBuffInfo(GetGroupUnitTagByIndex(playerID),i)
-				if ((abilityId == 109994) or (abilityId == 110020)) then
-					left = left + 1.3
-				end
-			end
-		end
-		if (GetUnitDisplayName(GetGroupUnitTagByIndex(playerID)) == AsylumOlorime.DPS7) then
-			for i=1,GetNumBuffs(GetGroupUnitTagByIndex(playerID)) do
-				local buffName, timeStarted, timeEnding, buffSlot, stackCount, iconFilename, buffType, effectType, abilityType, statusEffectType, abilityId, canClickOff, castByPlayer = GetUnitBuffInfo(GetGroupUnitTagByIndex(playerID),i)
-				if ((abilityId == 109994) or (abilityId == 110020)) then
-					left = left + 1.6
-				end
-			end
-		end
-		if (GetUnitDisplayName(GetGroupUnitTagByIndex(playerID)) == AsylumOlorime.DPS8) then
-			for i=1,GetNumBuffs(GetGroupUnitTagByIndex(playerID)) do
-				local buffName, timeStarted, timeEnding, buffSlot, stackCount, iconFilename, buffType, effectType, abilityType, statusEffectType, abilityId, canClickOff, castByPlayer = GetUnitBuffInfo(GetGroupUnitTagByIndex(playerID),i)
-				if ((abilityId == 109994) or (abilityId == 110020)) then
-					left = left + 1.9
+		for numDD = 1, 8 do 
+			if (GetUnitDisplayName(GetGroupUnitTagByIndex(playerID)) == AsylumOlorime.DPS[numDD]) then
+				for i = 1,GetNumBuffs(GetGroupUnitTagByIndex(playerID)) do
+					local buffName, timeS	tarted, timeEnding, buffSlot, stackCount, iconFilename, buffType, effectType, abilityType, statusEffectType, abilityId, canClickOff, castByPlayer = GetUnitBuffInfo(GetGroupUnitTagByIndex(playerID),i)
+					if ((abilityId == 109994) or (abilityId == 110020)) then
+						if numDD == 1 then
+							right = right + 1.9
+						elseif numDD == 2 then
+							right = right + 1.6
+						elseif numDD == 3 then
+							right = right + 1.3
+						elseif numDD == 4 then
+							right = right + 1
+						elseif numDD == 5 then
+							left = left + 1
+						elseif numDD == 6 then
+							left = left + 1.3
+						elseif numDD == 7 then
+							left = left + 1.6
+						elseif numDD == 8 then
+							left = left + 1.9
+						end
+					end
 				end
 			end
 		end
@@ -264,14 +229,8 @@ function AsylumOlorime:Initialize()
 	EVENT_MANAGER:UnregisterForEvent(AsylumOlorime.name, EVENT_ADD_ON_LOADED)
 	
 	--Update
-	AsylumOlorime.DPS1 = AsylumOlorime.savedVariables.DPS1
-	AsylumOlorime.DPS2 = AsylumOlorime.savedVariables.DPS2
-	AsylumOlorime.DPS3 = AsylumOlorime.savedVariables.DPS3
-	AsylumOlorime.DPS4 = AsylumOlorime.savedVariables.DPS4
-	AsylumOlorime.DPS5 = AsylumOlorime.savedVariables.DPS5
-	AsylumOlorime.DPS6 = AsylumOlorime.savedVariables.DPS6
-	AsylumOlorime.DPS7 = AsylumOlorime.savedVariables.DPS7
-	AsylumOlorime.DPS8 = AsylumOlorime.savedVariables.DPS8
+	AsylumOlorime.DPS = AsylumOlorime.savedVariables.DPS
+
 	EVENT_MANAGER:RegisterForUpdate(AsylumOlorime.name, 250, AsylumOlorime.Update)
 	EVENT_MANAGER:UnregisterForEvent(AsylumOlorime.name, EVENT_ADD_ON_LOADED)
 	
